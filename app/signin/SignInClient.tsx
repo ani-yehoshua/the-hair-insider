11bd3e928e0e2f9ffa16491d984a1ef3872fbab5
Joshua Ellis <xiixxxixcvi@gmail.com>
2026-03-11 18:17:09 -0500

    Add confirmation email redirect

--- app/signin/SignInClient.tsx
+++ app/signin/SignInClient.tsx
@@ -156,6 +156,9 @@ export default function SignInClient() {
             const { data, error } = await supabase.auth.signUp({
                 email,
                 password,
+                options: {
+                    emailRedirectTo: `${window.location.origin}/auth/callback`,
+                },
             });
             if (error) throw error;
 
